0e63f5f0c6fb097f56bdbc4e49308323b20e6afa
mikias-tulu <miki.tulu@gmail.com>
2024-06-25 08:00:00 +0300

    feat lib and utils for mongodb setup

---
+++ app/lib/mongodb.ts
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  connectTimeoutMS: 10000,
+};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo URI to .env");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
